8707776fc5c6e7506e957b7bd7b986d090151689
Robby Shaw <robbynshaw@gmail.com>
2020-03-24 18:13:09 -0500

    Converted style of inital pages to styled-c

---
+++ src/components/elements/Button.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding: 5px 10px;
+  background-color: #ffffff;
+  cursor: pointer;
+  font-weight: 600;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: #0070f3;
+    border-color: #0070f3;
+  }
+`;
+
+export default Button;
