888b68e70c3874e21c39ee6b6a73d122b8752349
Robby Shaw <robbynshaw@gmail.com>
2020-03-25 06:41:13 -0500

    Added missed file

--- src/pages/auth/connect/select/[stage].tsx
+++ src/pages/auth/connect/select/[stage].tsx
@@ -1,8 +1,14 @@
 import { NextPage } from "next";
 import { FunctionComponent } from "react";
-import CenteredEmptyLayout from "@layouts/CenteredEmpty";
-import { H1 } from "@src/components/elements/Headers";
 import { useRouter } from "next/router";
+import CenteredEmptyLayout from "@layouts/CenteredEmpty";
+import { H1 } from "@elements/Headers";
+import Card from "@elements/Card";
+import NavList from "@elements/NavList";
+import ListItemButton from "@elements/ListItemButton";
+import DropboxIcon from "@icons/DropboxIcon";
+import ComputerIcon from "@icons/ComputerIcon";
+import CloudIcon from "@icons/CloudIcon";
 
 const Select: NextPage = () => {
   const router = useRouter();
@@ -10,7 +16,23 @@ const Select: NextPage = () => {
   const isNew = stage === "new";
 
   const Content: FunctionComponent = () => (
-    <H1>Where {isNew ? "would you like your" : "is your"} data stored?</H1>
+    <>
+      <H1>Where {isNew ? "would you like your" : "is your"} data backed up?</H1>
+      <Card>
+        <NavList aria-label="main service selections">
+          <ListItemButton icon={DropboxIcon}>Dropbox</ListItemButton>
+          {/* <ListItemButton icon={CloudIcon}>Google Drive</ListItemButton>
+          <ListItemButton icon={CloudIcon}>Microsoft OneDrive</ListItemButton>
+          <ListItemButton icon={CloudIcon}>IPFS</ListItemButton>
+          <ListItemButton icon={CloudIcon}>DAT</ListItemButton>
+          <ListItemButton icon={CloudIcon}>WebDav</ListItemButton>
+          <ListItemButton icon={CloudIcon}>Local Server</ListItemButton> */}
+          <ListItemButton icon={ComputerIcon}>
+            Just in the browser (not recommended)
+          </ListItemButton>
+        </NavList>
+      </Card>
+    </>
   );
 
   return <CenteredEmptyLayout main={Content} />;
